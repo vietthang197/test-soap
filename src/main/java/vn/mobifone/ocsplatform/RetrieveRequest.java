a5ad82b005093029e231b8ef2216b025673adff9
Lê Việt Thắng <levietthang1997@gmail.com>
2021-05-20 13:41:33 +0700

    fix prefix response of spring ws

--- src/main/java/vn/mobifone/ocsplatform/RetrieveRequest.java
+++ src/main/java/vn/mobifone/ocsplatform/RetrieveRequest.java
@@ -11,11 +11,7 @@ package vn.mobifone.ocsplatform;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Generated;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 
 
 /**
